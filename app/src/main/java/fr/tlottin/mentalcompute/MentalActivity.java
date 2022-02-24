c934b14cc16944fe5ee6b0bc2703b5a54b07b144
Thomas LOTTIN <lottinthomas@hotmail.fr>
2022-02-24 01:27:06 +0100

    Randomization of calculations, display it by the text view

--- app/src/main/java/fr/tlottin/mentalcompute/MentalActivity.java
+++ app/src/main/java/fr/tlottin/mentalcompute/MentalActivity.java
@@ -8,21 +8,45 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class MentalActivity extends AppCompatActivity {
+
+    public enum Operation {
+        PLUS("+"),
+        MINUS("-"),
+        BY("*");
+
+        final String type;
+        Operation(String operator) {
+            this.type = operator;
+        }
+
+        String getOperator(){
+            return type;
+        }
+    }
+
+    private TextView operationText;
+    private int number1;
+    private int number2;
+    private Operation typeOperation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mental);
 
+        operationText = findViewById(R.id.operation);
         ImageButton home_imagebutton = findViewById(R.id.home_imageButton);
-        home_imagebutton.setOnClickListener(view -> goToPreviewActivity());
-    }
+        EditText editText = (EditText) findViewById(R.id.AnswerZone);
 
-    private void goToPreviewActivity() {
-        finish();
+        home_imagebutton.setOnClickListener(view -> goToPreviewActivity());
+        editText.requestFocus();
+
+        numbersAndOperationGenerator();
     }
 
     @Override
@@ -42,5 +66,33 @@ public class MentalActivity extends AppCompatActivity {
                 break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void goToPreviewActivity() {
+        finish();
+    }
+
+    private void numbersAndOperationGenerator() {
+        number1 = (int) (11*Math.random());
+        number2 = (int) (11*Math.random());
+
+        switch ((int) (3*Math.random())) {
+            case 0:
+                typeOperation = Operation.PLUS;
+                break;
+            case 1:
+                typeOperation = Operation.MINUS;
+                break;
+            case 2:
+                typeOperation = Operation.BY;
+                break;
+        }
+
+        displayOperation(number1, number2, typeOperation);
+    }
+
+    private void displayOperation(int n1, int n2, Operation typeOp) {
+        String compute = n1 + " " + typeOp.getOperator() + " " + n2;
+        operationText.setText(compute);
     }
 }
